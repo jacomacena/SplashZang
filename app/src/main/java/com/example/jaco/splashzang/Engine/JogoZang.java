7c2eccab67f422991bf7de454aafadaab27756c1
Jacó <jacog3@gmail.com>
2016-07-12 20:17:47 -0300

    Tela onde sera atualizado o SurfaceView do jogo

--- app/src/main/java/com/example/jaco/splashzang/Engine/JogoZang.java
+++ app/src/main/java/com/example/jaco/splashzang/Engine/JogoZang.java
@@ -1,7 +1,34 @@
 package com.example.jaco.splashzang.Engine;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 /**
  * Created by jaco on 12/07/16.
  */
-public class JogoZang {
+public class JogoZang extends Activity{
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        gameView.setGameIsRunning(false);
+        gameView.stopThread();
+        super.onDestroy();
+    }
 }
